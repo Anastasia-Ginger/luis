2df7b93e8531d7338aea041024cb61de84e7695f
Anastasia Ginger <kurenkova0301@gmail.com>
2021-01-19 00:02:36 +0200

    create section "about"

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#menu-yak").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
+
