aecd7ea8caa03a05f87f80c0bfb14a162f88b3e2
Anastasia Ginger <kurenkova0301@gmail.com>
2021-01-19 19:20:06 +0200

    added chart.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
+
 $(document).ready(function () {
     $("#menu-yak").on("click", "a", function (event) {
         event.preventDefault();
@@ -6,4 +25,3 @@ $(document).ready(function () {
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
-
